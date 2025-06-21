083b20c4ce1fce02e0d0315c256623ca665a97b2
Ifedolapo <ifemidolapooo@gmail.com>
2025-06-21 15:43:56 +0100

    Hero Section Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
 
       {/* Navbar */}
       <nav className="navbar">
-        <NavLink to="/#projects" className="nav-link">Projects</NavLink>
-        <NavLink to="/#about" className="nav-link">About</NavLink>
-        <NavLink to="/#contact" className="nav-link">Contact</NavLink>
+        <a href="#projects" className="nav-link">Projects</a>
+        <a href="#about" className="nav-link">About</a>
+        <a href="#contact" className="nav-link">Contact</a>
       </nav>
 
       {/* Bottom Line */}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,23 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-container">
-        
+        {/* Left:Intro text */}
+        <div className="hero-text">
+          <p className="intro-greeting">Welcome,</p>
+            <h1 className="hero-name">I'm Ifedolapo</h1>
+            <h2 className="hero-title">FRONT-END DEVELOPER</h2>
+            <p className="hero-summary">
+              Building applications that effectively convey your brand’s message and encourage visitors to engage — that’s my thing.<br/> 
+              I focus on creating intuitive, user-friendly experiences that look great and foster meaningful connections with your audience.
+            </p>
+            <a href="#contact" className="cta-link">
+              WORK WITH ME <span className="arrow">→</span>
+            </a>
+        </div>
+        {/* Right: Image */}
+        <div className="hero-image">
+          <img src="/images/hero1.jpg" alt="Hero Section Image" />
+        </div>
       </div>
     </section>
   );
