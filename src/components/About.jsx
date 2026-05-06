fbb16abd06b30add3efe7e4daa194f960ef717f2
Ifedolapo <ifemidolapooo@gmail.com>
2026-05-07 00:54:41 +0100

    Project Addition (Healing harbour)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
               and brings it to life with clarity and creativity.
             </p>
           <a
-            href="/ifedolapo-resume.pdf"
+            href="/ifedolapo-cv.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="about-cta"
